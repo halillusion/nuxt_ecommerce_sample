34127924f4793663e418ee9c1f4504da889c4188
halillusion <halillusion@gmail.com>
2022-10-01 04:11:26 +0300

    index page fixes.

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,55 @@
 <template>
-  <div>
-    <h1>Welcome to the homepage</h1>
+<Breadcrumb :schema="schema"/>
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <div class="content-wrapper">
+          <h1 class="page-title"><strong>Beyaz Eşya</strong> kategorisi içerisindeki ürünler listeleniyor</h1>
+          <div class="product-list">
+            <div class="row">
+              <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+                sdfsdf
+              </div>
+              <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+                sdfsdf
+              </div>
+              <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+                sdfsdf
+              </div>
+              <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+                sdfsdf
+              </div>
+              <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+                sdfsdf
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
+  
 </template>
+<script>
+export default {
+  name: 'Index',
+  data() {
+    return {
+      schema: [
+        {
+          "name": "Ana Sayfa",
+          "link": "/"
+        },
+        {
+          "name": "Ürünler",
+          "link": "/"
+        },
+        {
+          "name": "Beyaz Eşya",
+          "link": "/"
+        },
+      ]
+    }
+  }
+}
+</script>
