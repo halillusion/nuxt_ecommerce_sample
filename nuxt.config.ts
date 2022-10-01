9d60d469134bd5ae072762725f77663c9ed0361c
halillusion <halillusion@gmail.com>
2022-10-01 04:10:35 +0300

    nuxt config cdn links changed for libs folder.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,12 +16,13 @@ export default defineNuxtConfig({
         { name: "format-detection", content: "telephone=no" },
       ],
       script: [
-        { src: "https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" },
+        { src: "libs/bootstrap/bootstrap.bundle.min.js" },
       ],
       link: [
         { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
-        { rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Inter+Tight:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" },
-        { rel: "stylesheet", href: "https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" },
+        { rel: "stylesheet", href: "/libs/fonts.css" },
+        { rel: "stylesheet", href: "libs/bootstrap/bootstrap.min.css" },
+        { rel: "stylesheet", href: "libs/tabler-icons/tabler-icons.min.css" },
       ],
     },
   },
