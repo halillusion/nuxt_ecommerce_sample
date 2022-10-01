0373592759950a862f5a091f1ba7b517110b05c9
halillusion <halillusion@gmail.com>
2022-10-01 04:11:07 +0300

    Breadrumb component developments.

---
+++ src/components/Breadcrumb.vue
@@ -0,0 +1,27 @@
+<template>
+  <nav aria-label="breadcrumb" class="breadcrumb-bar">
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <ol class="breadcrumb">
+            <li v-for="(item, key) in schema" :key="key" class="breadcrumb-item" :class="(key+1) === schema.length ? 'active' : ''">
+              <NuxtLink v-if="(key+1) !== schema.length" :to="item.link">{{item.name}}</NuxtLink>
+              <strong v-else>{{item.name}}</strong>
+            </li>
+          </ol>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+<script>
+export default {
+  name: 'Breadcrumb',
+  props: {
+    schema: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
