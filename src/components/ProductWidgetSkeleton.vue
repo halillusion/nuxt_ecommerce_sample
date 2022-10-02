2ad941ccc2a23d587c65a6d88755aa9346c9f5f2
halillusion <halillusion@gmail.com>
2022-10-02 05:15:23 +0300

    ProductwidgetSkeleton component is added.

---
+++ src/components/ProductWidgetSkeleton.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="product-card skeleton" :class="layout">
+    <div class="image"></div>
+    <div class="body">
+      <div class="name"></div>
+      <div class="price"></div>
+      <div class="installment-price">
+        <strong></strong> 
+        <span></span>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'ProductWidgetskeleton',
+  props: {
+    layout: {
+      type: String,
+      enum: ['grid', 'list'],
+      required: true
+    }
+  }
+}
+</script>
