d8ad2f92ed660b49522b941b806c68a7d2198276
halillusion <halillusion@gmail.com>
2022-10-02 05:14:01 +0300

    AppFooter completed.

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,5 +1,76 @@
 <template>
-  <footer>
-    <p>© 2020</p>
+  <footer class="footer">
+    <div class="container">
+      <div class="row">
+        <div class="col-6 col-sm-6 col-md-4 col-lg-2">
+          <div class="menu">
+            <h6 class="menu-title">Kurumsal</h6>
+            <ul class="menu-list">
+              <li><a href="#">Hakkımızda</a></li>
+              <li><a href="#">Kampanyalar</a></li>
+              <li><a href="#">Bank Hesap Numaraları</a></li>
+              <li><a href="#">Blog</a></li>
+              <li><a href="#">Haberler</a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-6 col-sm-6 col-md-4 col-lg-2">
+          <div class="menu">
+            <h6 class="menu-title">Müşteri Hizmetleri</h6>
+            <ul class="menu-list">
+              <li><a href="#">Sipariş Sorgulama</a></li>
+              <li><a href="#">Teslimat ve Kargo</a></li>
+              <li><a href="#">İade ve Değişim</a></li>
+              <li><a href="#">Üyelik Sözleşmesi</a></li>
+              <li><a href="#">Gizlilik ve Güvenlik</a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-6 col-sm-6 col-md-4 col-lg-2">
+          <div class="menu">
+            <h6 class="menu-title">Yardım</h6>
+            <ul class="menu-list">
+              <li><a href="#">İletişim Bilgileri</a></li>
+              <li><a href="#">Sıkça Sorulan Sorular</a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-6 col-sm-6 col-md-6 col-lg-3">
+          <div class="menu">
+            <h6 class="menu-title">İletişim</h6>
+            <ul class="menu-list">
+              <li><strong>Adres:</strong>  Çınarlı, Martı Tower, Şht. Polis Fethi Sekin Cd No: 1 D:706, 35270 Konak/İzmir</li>
+              <li><strong>Telefon:</strong> 0850 455 1789</li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-12 col-sm-6 col-md-6 col-lg-3">
+          <div class="menu text-center text-sm-start">
+            <h6 class="menu-title">Bizi Takip Edin</h6>
+            <div class="social-links justify-content-center justify-content-sm-start">
+              <a href="#"><i class="ti ti-brand-facebook"></i></a>
+              <a href="#"><i class="ti ti-brand-twitter"></i></a>
+              <a href="#"><i class="ti ti-brand-youtube"></i></a>
+              <a href="#"><i class="ti ti-brand-instagram"></i></a>
+            </div>
+            <form class="bulletin">
+              <p>Bültenimize üye olun gelişmeleri kaçırmayın.</p>
+              <input type="text" placeholder="E-posta adresiniz" />
+              <button type="submit"><i class="ti ti-arrow-right"></i></button>
+            </form>
+          </div>
+        </div>
+      </div>
+      <hr />
+      <div class="row justify-content-between">
+        <div class="col-12">
+          <div class="footer-alt">
+            <p class="copyright">Telif Hakkı © 2020 NuxtCommerce</p>
+            <a class="powered" href="#"><img src="/img/copyright-logo.svg" alt="Copyright" /></a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <a href="#" class="whatsapp-btn"><i class="ti ti-brand-whatsapp"></i></a>
   </footer>
 </template>
